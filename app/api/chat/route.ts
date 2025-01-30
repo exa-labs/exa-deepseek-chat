3359890184c460d3005b1dafea8661f1b5d82c8b
dalist1 <gimitoci@gmail.com>
2025-01-30 15:17:28 +0000

    feat: smooth streaming

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,16 +1,20 @@
 import { deepseek } from '@ai-sdk/deepseek';
 import { fireworks } from '@ai-sdk/fireworks';
-import { streamText } from 'ai';
+import { streamText, smoothStream } from 'ai';
 
 export const maxDuration = 300;
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
-
+  
   const result = streamText({
     model: fireworks('accounts/fireworks/models/deepseek-r1'),
     system: "You are a helpful assistant that takes in the web for information and replies to the user with correct answer. Use simple english.",
     messages,
+    experimental_transform: smoothStream({
+      delayInMs: 20,
+      chunking: 'word',
+    }),
   });
 
   return result.toDataStreamResponse({ sendReasoning: true });
